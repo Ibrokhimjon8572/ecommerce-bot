7f3d667901a14f854d5f1bf2e158ce3aa5203c94
Mamajonov Ibrokhimjon <mamajonovibrokhimjon@gmail.com>
2023-03-25 20:58:37 +0500

    add user model

--- telegram/models.py
+++ telegram/models.py
@@ -1,3 +1,18 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, unique=True, editable=False)
+    language = models.CharField(max_length=2, null=False, default='uz')
+    user_id = models.CharField(max_length=20, null=False)
+    username = models.CharField(max_length=50, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=False)
+    name = models.CharField(max_length=30, null=True, blank=False)
+    is_admin = models.BooleanField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
