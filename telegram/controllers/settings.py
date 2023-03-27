f86aa41b4ad026f8ea4cabee7f40fb16b54a4866
Doniyorbek Urinboyev <doniyorbek7376@gmail.com>
2023-03-27 11:01:03 +0500

    settings button

---
+++ telegram/controllers/settings.py
@@ -0,0 +1,28 @@
+from telegram.control import Control, Handler, Displayer
+from telegram import keyboards
+from django.utils.translation import gettext as _
+from django.core.validators import RegexValidator
+import logging
+from django.core.exceptions import ValidationError
+
+
+class SettingsHandler(Handler):
+    def __init__(self, control: Control):
+        super().__init__(control)
+
+    def handle(self, text, message_id=None):
+        if text == _("back"):
+            self.user_session.state = 'main_menu'
+            self.user_session.save()
+            return
+        if text == _("change_language"):
+            self.user_session.state = 'select_language'
+            self.user_session.save()
+
+
+class SettingsDisplayer(Displayer):
+    def __init__(self, control: Control):
+        super().__init__(control)
+
+    def show(self):
+        self.reply(_("choose_action"), keyboards.settings_keyboard())
